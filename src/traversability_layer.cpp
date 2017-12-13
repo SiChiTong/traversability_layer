0219d883dc5356c2d84b441cc9ad474b3cdb6798
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-12-13 16:20:43 +0200

    Working costmap, still not optimized

--- src/traversability_layer.cpp
+++ src/traversability_layer.cpp
@@ -69,7 +69,6 @@ void TraversabilityLayer::updateBounds(double robot_x, double robot_y, double ro
         unsigned data_offset = input_tm_.data[index_of_interest].layout.data_offset; // zero for traversability
         unsigned index_x = input_tm_.data[index_of_interest].layout.dim[1].size; // assume row_index TODO add a check here
         unsigned index_y = input_tm_.data[index_of_interest].layout.dim[0].size; // assume column_index TODO add a check here
-        bool found = false;
         for(unsigned i = 0; i < input_tm_.data[index_of_interest].data.size(); i++){
             float cellv = input_tm_.data[index_of_interest].data[i];
             // If not NaN
@@ -89,19 +88,25 @@ void TraversabilityLayer::updateBounds(double robot_x, double robot_y, double ro
                     ROS_ERROR("mx:%u", mx);
                     ROS_ERROR("my:%u", my);
                     ROS_ERROR("Cell value:%f", cellv);
-                    // This work in reverse because all numbers in traversability map
-                    // appear to be negative
-                    setCost(mx, my, LETHAL_OBSTACLE*cellv);
-                    *min_x = std::min(*min_x, -200.0);
-                    *min_y = std::min(*min_y, -200.0);
-                    *max_x = std::max(*max_x, 200.0);
-                    *max_y = std::max(*max_y, 200.0);
+                    // Works with the latest (slightly customized!) traversability expression:
+                    // 1 - (1.0 / 3.0) * (traversability_slope + traversability_step + traversability_roughness)
+                    double cost = std::min(double(LETHAL_OBSTACLE), 2.0 * LETHAL_OBSTACLE * (cellv - 0.1));
+                    if (cost <= LETHAL_OBSTACLE / 2.0) {
+                        cost = 0.0;
+                    }
+                    setCost(mx, my, cost);
+                    // UNOPTIMIZED!
+                    *min_x = std::min(*min_x, -100.0);
+                    *min_y = std::min(*min_y, -100.0);
+                    *max_x = std::max(*max_x, 100.0);
+                    *max_y = std::max(*max_y, 100.0);
                     /*
-                    *min_x = std::min(*min_x, cellx);
-                    *min_y = std::min(*min_y, celly);
-                    *max_x = std::max(*max_x, cellx);
-                    *max_y = std::max(*max_y, celly);
+                    *min_x = std::min(*min_x, double(mx));
+                    *min_y = std::min(*min_y, double(my));
+                    *max_x = std::max(*max_x, double(mx));
+                    *max_y = std::max(*max_y, double(my));
                     */
+                    
                 }
             }
         }
@@ -109,8 +114,7 @@ void TraversabilityLayer::updateBounds(double robot_x, double robot_y, double ro
     }
 }
 
-void TraversabilityLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
-                                          int max_j){
+void TraversabilityLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j){
 
     for (int j = min_j; j < max_j; j++){
         for (int i = min_i; i < max_i; i++){
